1136be475dfa15a3db94a87d50811756b0429fd9
Stijn <stijn@stijnbakker.com>
2024-01-15 09:52:38 +0100

    Add weeknumber

--- Models/DayWeekModels.cs
+++ Models/DayWeekModels.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace tweekClone.Models
 {
@@ -15,5 +16,18 @@ namespace tweekClone.Models
     {
         public List<DayOverview> WeekDays { get; set; }
         public DateOnly StartDate { get; set; }
+        public string MonthName => StartDate.ToString("MMMM");
+        public int WeekNumber => GetWeekNumber(StartDate);
+
+        private int GetWeekNumber(DateOnly date)
+        {
+            var dateTime = date.ToDateTime(TimeOnly.FromDateTime(DateTime.Now));
+            Calendar calendar = DateTimeFormatInfo.CurrentInfo.Calendar;
+
+            return calendar.GetWeekOfYear(
+                dateTime,
+                CalendarWeekRule.FirstDay, // Or another rule depending on your needs
+                DayOfWeek.Sunday); // Or another day depending on your needs
+        }
     }
 }
